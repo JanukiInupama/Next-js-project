e1e338b6dd9c31930568b7f07c6086713b0677f8
Januki <januki.i@intervest.lk>
2024-08-12 11:53:49 +0530

    Resolved merge conflict in Banner.tsx

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,12 +1,7 @@
 const Banner = () => {
     return (
-<<<<<<< HEAD
         <div className="w-full flex justify-center py-6">
             <div className="bg-green-200 relative w-3/4 h-64 shadow-md sm:w-full">
-=======
-        <div className="w-full flex justify-center md:py-6">
-            <div className="bg-green-200 relative w-full md:w-3/4 h-40 md:h-64">
->>>>>>> 2291037 (basic structure)
 
             </div>
         </div>
