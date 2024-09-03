ad4d3f41d67f65eec6ea074a7f4ee89693d307b4
Pasindu Mendis <pasindu.m@intervest.lk>
2024-09-03 10:07:45 +0530

    add fixes

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import React, { useState } from 'react';
-import Image from 'next/image';
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleLike } from '../redux/likesSlice';
-import { RootState } from '../redux/store';
+import React, { useState } from "react";
+import Image from "next/image";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleLike } from "../redux/likesSlice";
+import { RootState } from "../redux/store";
 
-import Modal from './Modal';
+import Modal from "./Modal";
 
 type CardProps = {
   id: string;
@@ -16,8 +16,12 @@ type CardProps = {
 
 const Card = ({ id, imageSrc, altText }: CardProps) => {
   const dispatch = useDispatch();
-  const card = useSelector((state: RootState) => 
-    state.likes.cards.find(card => card.id === id) || { count: 0, liked: false }
+  const card = useSelector(
+    (state: RootState) =>
+      state.likes.cards.find((card) => card.id === id) || {
+        count: 0,
+        liked: false,
+      }
   );
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -29,11 +33,13 @@ const Card = ({ id, imageSrc, altText }: CardProps) => {
   const closeModal = () => setIsModalOpen(false);
 
   const CardContent = ({ inModal = false }) => (
-    <div className={`flex flex-col items-center ${inModal ? 'p-4' : ''}`}>
-      <div 
-        className={`relative ${inModal ? 'w-full h-64 md:h-80' : 'w-full h-96'} cursor-pointer`}
+    <div className={`flex flex-col items-center ${inModal ? "p-4" : ""}`}>
+      <div
+        className={`relative ${
+          inModal ? "w-full h-64 md:h-80" : "w-full h-96"
+        } cursor-pointer`}
         onClick={inModal ? undefined : openModal}
-      > 
+      >
         <Image
           src={imageSrc}
           alt={altText}
@@ -46,9 +52,11 @@ const Card = ({ id, imageSrc, altText }: CardProps) => {
       <div className="flex items-center space-x-4">
         <button
           onClick={handleLike}
-          className={`py-2 px-4 rounded ${card.liked ? 'bg-blue-700' : 'bg-blue-500'} hover:bg-blue-700 text-white font-bold`}
+          className={`py-2 px-4 rounded ${
+            card.liked ? "bg-blue-700" : "bg-blue-500"
+          } hover:bg-blue-700 text-white font-bold`}
         >
-          {card.liked ? 'Unlike' : 'Like'}
+          {card.liked ? "Unlike" : "Like"}
         </button>
         <span className="text-gray-700 font-semibold">{card.count} likes</span>
       </div>
@@ -60,7 +68,7 @@ const Card = ({ id, imageSrc, altText }: CardProps) => {
       <div className="bg-white shadow-md rounded-lg p-5 h-full flex flex-col">
         <CardContent />
       </div>
-  
+
       <Modal isOpen={isModalOpen} onClose={closeModal}>
         <CardContent inModal={true} />
       </Modal>
